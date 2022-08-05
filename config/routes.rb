97250c1e4fded990636a9ee3c6c8e5a8bc235e52
Aniede Chibundu David <ndubest56@gmail.com>
2022-08-05 14:42:55 +0100

    initial commit, enjoyed working with Rails

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "home#index"
+  # root "friends#index"
+  get 'home/about'
+  get 'display' => 'friends#display'
+  root 'friends#display'
+  
+
+end
